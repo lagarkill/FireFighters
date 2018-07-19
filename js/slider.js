91d54f0e69ca4304ae86999a5323806dc0c95a0c
lagarkill <apachai.ho@hotmail.com>
2018-07-19 13:48:48 -0300

    reponsive

--- js/slider.js
+++ js/slider.js
@@ -5,19 +5,19 @@ var button = $('#asdasd');
 
 // Slider functions
 
-$('#slider div:last').insertBefore('#slider div:first');
+$('#slider section:last').insertBefore('#slider section:first');
 
 slider.css('margin-left','-'+100+'vw');
 
 function moveL(){
     slider.animate({marginLeft:'-'+200+'vw'},700 , function(){
-        $('#slider div:first').insertAfter('#slider div:last');
+        $('#slider section:first').insertAfter('#slider section:last');
         slider.css('margin-left','-'+100+'vw');
     })
 }
 function moveR(){
     slider.animate({marginLeft:0},700 , function(){
-        $('#slider div:last').insertBefore('#slider div:first');
+        $('#slider section:last').insertBefore('#slider section:first');
         slider.css('margin-left','-'+100+'vw');
     })
 }
